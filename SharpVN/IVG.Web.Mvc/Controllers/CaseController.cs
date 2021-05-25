e88628fe4aa8fbf4d0bd7a4ca5539f54c9df768c
dongta <dongnv.it@gmail.com>
2021-05-25 20:36:15 +0700

    mỏi mỏi quá

--- SharpVN/IVG.Web.Mvc/Controllers/CaseController.cs
+++ SharpVN/IVG.Web.Mvc/Controllers/CaseController.cs
@@ -1,6 +1,9 @@
 ﻿using IVG.Web.Mvc.EF;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -14,6 +17,47 @@ namespace IVG.Web.Mvc.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public FileContentResult GetJob()
+        {
+            Response.Clear();
+            var listJob = db.tbl_Cases.ToList();
+            ExcelPackage excel = new ExcelPackage();
+            var workSheet = excel.Workbook.Worksheets.Add("Sheet1");
+            workSheet.TabColor = System.Drawing.Color.Black;
+            workSheet.DefaultRowHeight = 12;
+            workSheet.Row(1).Height = 20;
+            workSheet.Row(1).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+            workSheet.Row(1).Style.Font.Bold = true;
+            workSheet.Cells[1, 1].Value = "S.No";
+            workSheet.Cells[1, 2].Value = "Id";
+            workSheet.Cells[1, 3].Value = "Name";
+            workSheet.Cells[1, 4].Value = "Address";
+            int recordIndex = 2;
+            foreach (var job in listJob)
+            {
+                workSheet.Cells[recordIndex, 1].Value = (recordIndex - 1).ToString();
+                workSheet.Cells[recordIndex, 2].Value = job.CaseCode;
+                workSheet.Cells[recordIndex, 3].Value = job.SerialNo;
+                workSheet.Cells[recordIndex, 4].Value = job.ApprovalDate;
+                recordIndex++;
+            }
+            workSheet.Column(1).AutoFit();
+            workSheet.Column(2).AutoFit();
+            workSheet.Column(3).AutoFit();
+            workSheet.Column(4).AutoFit();
+            string excelName = $"Job-Export-{DateTime.Now.ToString("ddMMyyyy")}";
+            return new FileContentResult(excel.GetAsByteArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+            //using (var memoryStream = new MemoryStream())
+            //{
+            //    Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+            //    Response.AddHeader("content-disposition", "attachment; filename=" + excelName + ".xlsx");
+            //    excel.SaveAs(memoryStream);
+            //    memoryStream.WriteTo(Response.OutputStream);
+            //    Response.Flush();
+            //    Response.End();
+            //}
         }
     }
 }
